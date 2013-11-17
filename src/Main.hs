98a0b3880bdb94819385dfdf370ff75387ffd831
Jean-Alexandre Peyroux <m@j4.pe>
2013-11-18 00:18:20 +0100

    Modif du Main pour return js

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import           Control.Applicative
@@ -6,6 +7,7 @@ import           Snap.Core
 import           Snap.Util.FileServe
 import           Snap.Http.Server
 
+import qualified Data.ByteString.Char8 as BS
 import Control.Monad.IO.Class (liftIO)
 import 			 Reverseme
 
@@ -23,6 +25,7 @@ site =
 appendHandler :: Snap ()
 appendHandler = do
     param <- getParam "appendparam"
-    liftIO $ writeNginxConf
+    case param of Just param' -> liftIO $ writeNginxConf $ BS.unpack param'
+    	          Nothing -> writeBS "no no no !"
     maybe (writeBS "must specify echo/param in URL")
           writeBS param
