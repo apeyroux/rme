f966183e7e95ce6af3a725f70afa33e15f32cc44
Jean-Alexandre Peyroux <m@j4.pe>
2013-11-18 10:53:07 +0100

    ajout du delete d'un vhost

--- src/Main.hs
+++ src/Main.hs
@@ -9,7 +9,8 @@ import           Snap.Http.Server
 
 import qualified Data.ByteString.Char8 as BS
 import Control.Monad.IO.Class (liftIO)
-import 			 Reverseme
+import Control.Exception (SomeException)
+import Reverseme
 
 main :: IO ()
 main = quickHttpServe site
@@ -17,15 +18,29 @@ main = quickHttpServe site
 site :: Snap ()
 site =
     ifTop (writeBS "{\"message\":\"404 not found !\"}") <|>
-    route [ ("foo", writeBS "bar")
-          , ("append/:appendparam", appendHandler)
+    route [ --("foo", writeBS "bar"),
+          ("append/:appendparam", appendHandler)
+          , ("remove/:removeparam", removeHandler)
           ] <|>
     dir "static" (serveDirectory ".")
 
 appendHandler :: Snap ()
 appendHandler = do
     param <- getParam "appendparam"
-    case param of Just param' -> liftIO $ writeNginxConf $ BS.unpack param'
+    case param of Just param' -> liftIO $ appendNginxVhost $ BS.unpack param'
     	          Nothing -> writeBS "no no no !"
+    maybe (writeBS "must specify echo/param in URL")
+          writeBS param
+
+invalidJson :: SomeException -> Snap ()
+invalidJson ex = do
+  writeBS "param"
+  
+removeHandler :: Snap ()
+removeHandler = do 
+    param <- getParam "removeparam"
+    case param of 
+      Just param' ->
+          liftIO $ removeNginxVhost $ BS.unpack param'
     maybe (writeBS "must specify echo/param in URL")
           writeBS param
