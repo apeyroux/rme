007ddd815c450f1d1c8b290f735e6af64220b259
Jean-Alexandre Peyroux <m@j4.pe>
2013-11-19 23:50:44 +0100

    M Propre !

--- src/Main.hs
+++ src/Main.hs
@@ -2,30 +2,43 @@
 
 module Main where
 
-import           Control.Applicative
-import           Snap.Core
-import           Snap.Http.Server
-import           Snap.Util.FileServe
-import Prelude hiding (catch)
-import qualified Control.Exception as C
-
--- import           Control.Exception.Lifted (handle)
--- import           Control.Exception      (SomeException)
--- import Control.Monad.CatchIO
-import Control.Exception.Base
+import Snap.Core
+import Data.Aeson
+import Snap.Http.Server
+import Control.Applicative
+import Snap.Util.FileServe
+import System.Cmd (rawSystem)
 import Control.Monad.IO.Class (liftIO)
-import qualified Data.ByteString.Char8  as BS
+import Control.Exception.Lifted (handle)
+import Control.Exception (SomeException, catch)
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy as BL
+
 import Reverseme
+
+nginxConfiguration = NginxConfiguration "/etc/nginx/sites-enabled/" "/etc/init.d/nginx"
+
+data JsonMessage = JsonMessage {
+  mAction :: String,
+  mMessage :: String,
+  mVhostDirectory :: String
+} deriving (Eq, Show)
+
+instance ToJSON JsonMessage where
+  toJSON (JsonMessage a m p) = object ["message" .= m,
+    "action" .= a,
+    "direcrory" .= p]
 
 main :: IO ()
 main = quickHttpServe site
 
 site :: Snap ()
 site =
-    ifTop (writeBS "{\"message\":\"404 not found !\"}") <|>
+    ifTop (writeBS "STSI2/SDAC/BCM") <|>
     route [ --("foo", writeBS "bar"),
-          ("append/:appendparam", appendHandler)
-          , ("remove/:removeparam", removeHandler)
+          ("append/:appendparam", appendHandler),
+          ("drop/:dropparam", dropHandler),
+          ("push", pushHandler)
           ] <|>
     dir "static" (serveDirectory ".")
 
@@ -33,19 +46,39 @@ exceptionCfg :: SomeException -> Snap ()
 exceptionCfg ex = do
   writeBS "no non non !"
 
+vHost :: BS.ByteString -> NginxVhost
+vHost param = vh 
+  where
+    (Just vh) = decode (BL.fromStrict param)::Maybe NginxVhost
+
+pushHandler :: Snap ()
+pushHandler = do
+  liftIO $ ngxReload nginxConfig
+  writeBS $ BL.toStrict jsMessage
+  where
+    nginxConfig = nginxConfiguration
+    jsMessage = encode $ JsonMessage "push" "push modif" ""
+
+dropHandler :: Snap ()
+dropHandler = do
+  param <- getParam "dropparam"
+  case param of
+    Just param' -> do 
+      liftIO $ ngxDropVHost nginxConfig (vHost param')
+      liftIO $ ngxReload nginxConfig
+      writeBS $ BL.toStrict jsMessage
+  where
+    nginxConfig = nginxConfiguration
+    jsMessage = encode $ JsonMessage "drop" "drop vhost" ""
+
 appendHandler :: Snap ()
 appendHandler = do
     param <- getParam "appendparam"
-    case param of 
-      Just param' -> C.catch (createConfig (BS.unpack param')) exceptionCfg
-    maybe (writeBS "must specify echo/param in URL") writeBS param
-
-removeHandler :: Snap ()
-removeHandler = do
-  param <- getParam "removeparam"
-  case  param of
-    Just param' ->
-      liftIO $ removeNginxVhost $ BS.unpack param'
-
-  maybe (writeBS "must specify echo/param in URL")
-    writeBS param
+    case param of
+      Just param' -> do 
+        liftIO $ ngxWriteVHost nginxConfig (vHost param')
+        liftIO $ ngxReload nginxConfig
+        writeBS $ BL.toStrict jsMessage
+  where
+    nginxConfig = nginxConfiguration
+    jsMessage = encode $ JsonMessage "append" "append vhost" ""
